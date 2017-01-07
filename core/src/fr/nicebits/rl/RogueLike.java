477f1638bd6a664435f8a29022868ac9fe207372
MinusKube <minuskube@gmail.com>
2017-01-07 04:50:50 +0100

    Just testing a thing?

--- core/src/fr/nicebits/rl/RogueLike.java
+++ core/src/fr/nicebits/rl/RogueLike.java
@@ -19,7 +19,6 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2D;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
